d592c55667ee62decd3a01913b8f9f4d1e7c8ed9
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2021-04-30 18:52:54 +0300

    create Layout component

---
+++ frontend/src/components/Layout/index.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from 'components/Navbar';
+import React from 'react';
+import styles from './styles.module.sass';
+
+export const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.main}>{children}</div>
+    </div>
+  );
+};
