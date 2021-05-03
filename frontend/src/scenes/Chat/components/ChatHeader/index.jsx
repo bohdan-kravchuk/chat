76112de983cd18c7cdfa0415ace801e3206a8417
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2021-05-03 17:43:02 +0300

    implement ChatHeader

--- frontend/src/scenes/Chat/components/ChatHeader/index.jsx
+++ frontend/src/scenes/Chat/components/ChatHeader/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './styles.module.sass';
 
-export const ChatHeader = () => {
-  return <div className={styles.chatHeader}></div>;
+export const ChatHeader = ({ name, description, avatarURI }) => {
+  return (
+    <div className={styles.chatHeader}>
+      <div className={styles.avatarWrapper}>
+        <img src={avatarURI} alt="avatar" className={styles.avatar} />
+      </div>
+      <div className={styles.info}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  );
 };
--- frontend/src/scenes/Chat/containers/Chat/index.jsx
+++ frontend/src/scenes/Chat/containers/Chat/index.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { ChatBody } from 'scenes/Chat/components/ChatBody';
 import { ChatHeader } from 'scenes/Chat/components/ChatHeader';
 import styles from './styles.module.sass';
 
 export const Chat = () => {
+  // @ts-ignore
+  const activeChat = useSelector(({ chat }) => {
+    if (chat.activeId) {
+      return chat.entities[chat.activeId];
+    }
+  });
+  // @ts-ignore
+  const currentUser = useSelector(({ auth }) => auth.currentUser);
+  // @ts-ignore
+  const companion = useSelector(({ user }) => {
+    const [firstUserId, secondUserId] = activeChat?.users ?? [];
+    const companionId = firstUserId === currentUser.id ? secondUserId : firstUserId;
+    return user.entities[companionId];
+  });
+
   return (
     <div className={styles.chat}>
-      <ChatHeader />
-      <ChatBody />
+      {activeChat && (
+        <>
+          <ChatHeader
+            name={companion.userName}
+            avatarURI={companion.avatarURI}
+            description={companion.bio}
+          />
+          <ChatBody />
+        </>
+      )}
     </div>
   );
 };
