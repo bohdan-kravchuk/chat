692ff8cb564810d1411b565c9e65633405ee8953
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2021-05-04 13:42:48 +0300

    implement search chat logic

--- frontend/src/scenes/Chat/containers/ChatSidebar/index.jsx
+++ frontend/src/scenes/Chat/containers/ChatSidebar/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { useDebounce } from 'hooks/useDebouce';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useCallback } from 'react';
 import { FormControl } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { ChatItem } from 'scenes/Chat/components/ChatItem';
@@ -6,7 +8,10 @@ import { ChatSidebarTabs } from 'scenes/Chat/components/ChatSidebarTabs';
 import { selectChat } from 'state/chatSlice';
 import styles from './styles.module.sass';
 
+const delay = 500;
+
 export const ChatSidebar = () => {
+  const [searchText, setSearchText] = useState('');
   // @ts-ignore
   const chatsByIds = useSelector(({ chat }) => chat.entities);
   // @ts-ignore
@@ -15,14 +20,40 @@ export const ChatSidebar = () => {
   const usersByIds = useSelector(({ user }) => user.entities);
   // @ts-ignore
   const currentUser = useSelector(({ auth }) => auth.currentUser);
-
   const dispatch = useDispatch();
+  const debouncedSearchText = useDebounce(searchText, delay);
+  const chats = useMemo(() => Object.values(chatsByIds), [chatsByIds]);
+  const [filteredChats, setFilteredChats] = useState(chats);
+
+  const filterChatsByName = useCallback(
+    (name) => {
+      const result = name
+        ? chats.filter((item) => {
+            const [firstUserId, secondUserId] = item.users;
+            const companionId =
+              firstUserId === currentUser.id ? secondUserId : firstUserId;
+            const companion = usersByIds[companionId];
+            return companion.userName.toLowerCase().includes(name);
+          })
+        : chats;
+      setFilteredChats(result);
+    },
+    [usersByIds, currentUser.id, chats]
+  );
+
+  useEffect(() => {
+    filterChatsByName(debouncedSearchText.trim().toLowerCase());
+  }, [debouncedSearchText, filterChatsByName]);
 
   const onChatSelect = (id) => {
     dispatch(selectChat({ id }));
   };
 
-  const chatList = Object.values(chatsByIds).map(({ id, users }) => {
+  const onChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const chatList = filteredChats.map(({ id, users }) => {
     const [firstUserId, secondUserId] = users;
     const companionId = firstUserId === currentUser.id ? secondUserId : firstUserId;
     const { avatarURI, userName, isOnline, info } = usersByIds[companionId];
@@ -46,7 +77,7 @@ export const ChatSidebar = () => {
       <div className={styles.tabContent}>
         <div className={styles.chatList}>{chatList}</div>
         <div className={styles.searchWrapper}>
-          <FormControl placeholder="Search..." />
+          <FormControl placeholder="Search..." value={searchText} onChange={onChange} />
         </div>
       </div>
     </div>
